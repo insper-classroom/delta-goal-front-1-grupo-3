62e7c686adab6a1f308b03d1d6ff74b51e8db0c9
Luigi Quinze <luigioq@al.insper.edu.br>
2023-11-23 14:19:29 -0300

    feat: sessionStorage para testes e Header.js

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Header from './Header'; // Ajuste o caminho do import conforme necessário
 
-export default function Home() {
-  return(
-    <h2>Home</h2>
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      {/* Conteúdo da Home */}
+    </div>
   );
 }
+
+export default Home;
