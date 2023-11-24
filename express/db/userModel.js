e9382f97c79e12dfec140addaecd6e5361bc8ca7
leosfreitas <leonardosf@al.insper.edu.br>
2023-11-24 09:32:54 -0300

    Express

---
+++ express/db/userModel.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({
+    email : {
+        type : String,
+        required : [true, 'Email is required'],
+        unique : [true, 'Email already exists'],
+    },
+
+
+    password : {
+        type : String,
+        required : [true, 'Password is required'],
+        unique : false
+    },
+    })
+
+module.exports = mongoose.model.Users || mongoose.model('Users', userSchema);
