53ae7fcecd1f0d2871c150c76ae95f4fcffcd805
leosfreitas <leonardosf@al.insper.edu.br>
2023-11-28 16:13:47 -0300

    Verificação

--- front-end/src/ProtectedRoutes.js
+++ front-end/src/ProtectedRoutes.js
@@ -1,16 +1,39 @@
+import React, { useEffect, useState } from 'react';
 import { useLocation, Navigate, Outlet } from 'react-router-dom';
 import Cookies from 'universal-cookie';
-import React from 'react';
+import axios from 'axios';
+
 const cookies = new Cookies();
 
 export default function ProtectedRoutes() {
-  const isAuthenticated = cookies.get('token');
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   const location = useLocation();
 
+  const checkLogin = async () => {
+    try {
+      const token = cookies.get('token');
+      if (token) {
+        const response = await axios.post('http://localhost:8080/verificar-login', {
+          token,
+      });
+        console.log('Response:', response);
+        if (response.status === 200 && response.data.message === 'Autorizado') {
+          setIsAuthenticated(true);
+        } else {
+          console.log('Falha na autenticação:', response.data.message);
+        }
+      }
+    } catch (error) {
+      console.error('Erro durante a verificação de autenticação:', error);
+      setIsAuthenticated(false);
+    }
+  };
+  useEffect(() => {
+    checkLogin();
+  }, []);
   return (
-    isAuthenticated ? 
-        <Outlet /> 
+    isAuthenticated ?
+      <Outlet />
       : <Navigate to={{ pathname: '/login', state: { from: location } }} />
-      );
-
+  );
 }
--- front-end/src/pages/Logout.js
+++ front-end/src/pages/Logout.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import Cookies from 'universal-cookie';
 import { useNavigate } from 'react-router-dom';
-
+import axios from 'axios';
 const cookies = new Cookies();
 
 const Logout = () => {
     const navigate = useNavigate();
+    const token = cookies.get('token')
 
-    const handleLogout = () => {
-        cookies.remove('token', {path: '/'});
-        sessionStorage.removeItem('token');
-        navigate('/login');
+    const handleLogout = (e) => {
+        e.preventDefault();
+        axios
+        .post(('http://localhost:8080/logout'), {
+            token 
+        })
+        .then((response) => { 
+            console.log(response)
+            cookies.remove('token', {path: '/'});
+            sessionStorage.removeItem('token');
+            navigate('/login');
+        })
+        .catch((error) => {
+            console.log(error)
+        })
     };
 
     return (
