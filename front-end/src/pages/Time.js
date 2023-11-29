a142df9ea04185b23eae45d6cae16796dadf1af3
nina <nina.savoy@icloud.com>
2023-11-29 11:36:47 -0300

    basesinha ainda vou fazer as requisicoes

--- front-end/src/pages/Time.js
+++ front-end/src/pages/Time.js
@@ -7,18 +7,123 @@ export default function Time() {
       <div>
         <Header />
         <h1>
-            Times
-          </h1>
-          <div class="times">
-            <div class="bragantino">
+        Times
+        </h1>
+        <div class="times">
+          <div class="bragantino">
+            <div class="titulo">
               <img src='bragantino logo.png' alt='bragantino'/>
               <a href="x">Bragantino</a>
             </div>
-            <div class="palmeiras">
+            <div class="box">
+              <div class="jogadores">
+                <h1>Jogadores</h1>
+                {/* eu nao sei como fazer... soh uma tentativa do chat */}
+                <ul>
+                  {Object.entries(teams).map(([teamName, teamInfo]) => (
+                    <li key={teamName}>
+                      <strong>{teamName}</strong>: {JSON.stringify(teamInfo)}
+                    </li>
+                  ))}
+                </ul>
+                <ul>
+                  {Object.entries(teams).map(([teamName, teamInfo]) => (
+                    <li key={teamName}>
+                      <strong>{teamName}</strong>: {JSON.stringify(teamInfo)}
+                    </li>
+                  ))}
+                </ul>
+                <ul>
+                  {Object.entries(teams).map(([teamName, teamInfo]) => (
+                    <li key={teamName}>
+                      <strong>{teamName}</strong>: {JSON.stringify(teamInfo)}
+                    </li>
+                  ))}
+                </ul>
+                <ul>
+                  {Object.entries(teams).map(([teamName, teamInfo]) => (
+                    <li key={teamName}>
+                      <strong>{teamName}</strong>: {JSON.stringify(teamInfo)}
+                    </li>
+                  ))}
+                </ul>
+                <ul>
+                  {Object.entries(teams).map(([teamName, teamInfo]) => (
+                    <li key={teamName}>
+                      <strong>{teamName}</strong>: {JSON.stringify(teamInfo)}
+                    </li>
+                  ))}
+                </ul>
+                <ul>
+                  {Object.entries(teams).map(([teamName, teamInfo]) => (
+                    <li key={teamName}>
+                      <strong>{teamName}</strong>: {JSON.stringify(teamInfo)}
+                    </li>
+                  ))}
+                </ul>
+                <ul>
+                  {Object.entries(teams).map(([teamName, teamInfo]) => (
+                    <li key={teamName}>
+                      <strong>{teamName}</strong>: {JSON.stringify(teamInfo)}
+                    </li>
+                  ))}
+                </ul>
+                <ul>
+                  {Object.entries(teams).map(([teamName, teamInfo]) => (
+                    <li key={teamName}>
+                      <strong>{teamName}</strong>: {JSON.stringify(teamInfo)}
+                    </li>
+                  ))}
+                </ul>
+                <ul>
+                  {Object.entries(teams).map(([teamName, teamInfo]) => (
+                    <li key={teamName}>
+                      <strong>{teamName}</strong>: {JSON.stringify(teamInfo)}
+                    </li>
+                  ))}
+                </ul>
+                <ul>
+                  {Object.entries(teams).map(([teamName, teamInfo]) => (
+                    <li key={teamName}>
+                      <strong>{teamName}</strong>: {JSON.stringify(teamInfo)}
+                    </li>
+                  ))}
+                </ul>
+                <ul>
+                  {Object.entries(teams).map(([teamName, teamInfo]) => (
+                    <li key={teamName}>
+                      <strong>{teamName}</strong>: {JSON.stringify(teamInfo)}
+                    </li>
+                  ))}
+                </ul>
+                <ul>
+                  {Object.entries(teams).map(([teamName, teamInfo]) => (
+                    <li key={teamName}>
+                      <strong>{teamName}</strong>: {JSON.stringify(teamInfo)}
+                    </li>
+                  ))}
+                </ul>
+                <ul>
+                  {Object.entries(teams).map(([teamName, teamInfo]) => (
+                    <li key={teamName}>
+                      <strong>{teamName}</strong>: {JSON.stringify(teamInfo)}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div class="coisinhas">
+                <h3>Rupturas</h3>
+                <h3>Cruzamentos</h3>
+              </div>
+            </div>
+          </div>
+          <div class="palmeiras">
+            <div class="titulo">
               <img src='palmeiras.png' alt='palmeiras'/>
               <a href="x">Palmeiras</a>
             </div>
           </div>
+        </div>
       </div>
   );
 }
