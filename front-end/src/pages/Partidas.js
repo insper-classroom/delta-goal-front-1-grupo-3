91bd9e8e510955b3ba3f94fba4bb610add5883f0
jonasbp <jonasbonfapelegrina@gmail.com>
2023-11-29 17:36:31 -0300

    Faz requisição com o token

--- front-end/src/pages/Partidas.js
+++ front-end/src/pages/Partidas.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import Header from './Header';
 import './style/Partidas.css';
+import Cookies from 'js-cookie';
+
 
 export default function Partidas() {
   const [lances, setLances] = useState([]);
@@ -10,47 +12,32 @@ export default function Partidas() {
   useEffect(() => {
     const fetchLances = async () => {
       try {
-        const responseLances = await fetch("https://sprint-deltago-5179309dcfcb.herokuapp.com/rupturas_jogadores/jogo/Palmeiras x Red Bull Bragantino/time/Red Bull Bragantino", {
+        // Obter todos os cookies do navegador
+        const token = Cookies.get('token');
+        console.log(token)
+        const response = await fetch("https://sprint-deltago-5179309dcfcb.herokuapp.com/rupturas_jogadores/jogo/Palmeiras x Red Bull Bragantino/time/Red Bull Bragantino", {
           method: 'GET',
           headers: {
             "Content-Type": "application/json",
-            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcwMTI4ODEzMSwianRpIjoiYjlhNGNjNTUtNTdhMS00ZTVkLWI0ZWItMTJhMzc4NTE4ZjVlIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6IjY1Njc0YjE5ODkwZmRmZjM0MmMyYzFkYSIsIm5iZiI6MTcwMTI4ODEzMSwiZXhwIjoxNzAxMjg5MDMxfQ.1-2WisDml4jkzS44IEsBM-x_E_M03cP4DlmCa1yqUQg",
+            "Authorization": `Bearer ${token}`,
           },
           redirect: 'follow',
         });
 
-        if (responseLances.ok) {
-          const data = await responseLances.json();
+        if (response.ok) {
+          const data = await response.json();
           console.log(data);
           setLances(data);
         } else {
-          setError(`Erro ao buscar lances: ${responseLances.statusText}`);
-        }
-        const response_lista_rupturas= await fetch("https://sprint-deltago-5179309dcfcb.herokuapp.com/rupturas_lista/jogo/Palmeiras x Red Bull Bragantino/time/Red Bull Bragantino", {
-          method: 'GET',
-          headers: {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcwMTI4OTAyNCwianRpIjoiYTI4ZDJjM2MtNjg0ZC00OGY1LThjYjMtMjc3Y2Y5YmFkMWI1IiwidHlwZSI6ImFjY2VzcyIsInN1YiI6IjY1Njc0YjE5ODkwZmRmZjM0MmMyYzFkYSIsIm5iZiI6MTcwMTI4OTAyNCwiZXhwIjoxNzAxMjg5OTI0fQ.Gq9BXAdD-S9mW1h1QrXBVCJRlpNjTj0W2K7jWZnwu8Y",
-          },
-          redirect: 'follow',
-        });
-
-        if (response_lista_rupturas.ok) {
-          const data2 = await response_lista_rupturas.json();
-          console.log(data2);
-          setLances(data2);
-        } else {
-          setError(`Erro ao buscar lances: ${response_lista_rupturas.statusText}`);
+          setError(`Erro ao buscar lances: ${response.statusText}`);
         }
       } catch (error) {
         setError(`Erro na requisição: ${error.message}`);
       } finally {
         setIsLoading(false);
       }
-    
     };
 
-    // Faz todas as requisições
     fetchLances();
   }, []);
 
@@ -71,7 +58,6 @@ export default function Partidas() {
             <div className="lines"></div>
           </div>
           <div className="destaques">Destaques</div>
-          response_lista_rupturas: {JSON.stringify(lances)}
           <div className="envolvidos">Jogadores Envolvidos</div>
           <div className="desfechos">Desfechos</div>
         </div>
