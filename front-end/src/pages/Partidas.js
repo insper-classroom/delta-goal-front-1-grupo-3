7ea338df973695cf0333cc5a55448728504460b2
leosfreitas <leonardosf@al.insper.edu.br>
2023-12-01 14:37:15 -0300

    .

--- front-end/src/pages/Partidas.js
+++ front-end/src/pages/Partidas.js
@@ -4,48 +4,97 @@ import './style/Partidas.css';
 import Cookies from 'universal-cookie';
 const cookies = new Cookies();
 
-export default function Partidas() {
-  const [lances, setLances] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
+  export default function Partidas() {
+    const [rupturas, setRupturas] = useState([]);
+    const [destaques, setDestaques] = useState([]);
+    const [desfechos, setDesfechos] = useState([]); 
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const token = cookies.get('token');
 
-  useEffect(() => {
-    const fetchLances = async () => {
-      try {
-        // Obter todos os cookies do navegador
-        const token = cookies.get('token');
-        console.log(token)
-        const response = await fetch("https://sprint-deltago-5179309dcfcb.herokuapp.com/rupturas_jogadores/jogo/Palmeiras x Red Bull Bragantino/time/Red Bull Bragantino", {
-          method: 'GET',
-          headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${token}`,
-          },
-          redirect: 'follow',
-        });
-
-        if (response.ok) {
-          const data = await response.json();
-          console.log(data);
-          setLances(data);
-        } else {
-          setError(`Erro ao buscar lances: ${response.statusText}`);
+    useEffect(() => {
+      const fetchDestaques = async () => {
+        try {
+          const response = await fetch("https://sprint-deltago-5179309dcfcb.herokuapp.com/rupturas_jogadores/jogo/Palmeiras x Red Bull Bragantino/time/Red Bull Bragantino", {
+            method: 'GET',
+            headers: {
+              "Content-Type": "application/json",
+              "Authorization": `Bearer ${token}`,
+            },
+            redirect: 'follow',
+          });
+          if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            setDestaques(data);
+          } else {
+            setError(`Erro ao buscar destaques: ${response.statusText}`);
+          }
+        } catch (error) {
+          setError(`Erro na requisição: ${error.message}`);
+        } finally {
+          setIsLoading(false);
         }
-      } catch (error) {
-        setError(`Erro na requisição: ${error.message}`);
-      } finally {
-        setIsLoading(false);
-      }
-    };
+      };
 
-    fetchLances();
-  }, []);
+      const fetchLances = async () => {
+        try {
+            const response = await fetch("https://sprint-deltago-5179309dcfcb.herokuapp.com/rupturas_lista/jogo/Palmeiras x Red Bull Bragantino/time/Red Bull Bragantino", {
+            method: 'GET',
+            headers: {
+              "Content-Type": "application/json",
+              "Authorization": `Bearer ${token}`,
+            },
+            redirect: 'follow',
+          });
+
+          if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            setRupturas(data);
+          } else {
+            setError(`Erro ao buscar lances: ${response.statusText}`);
+          }
+        } catch (error) {
+          setError(`Erro na requisição: ${error.message}`);
+        } finally {
+          setIsLoading(false);
+        }
+      };
+
+      const fetchDesfechos = async () => {
+        try {
+          const response = await fetch("https://sprint-deltago-5179309dcfcb.herokuapp.com/rupturas_grafico/jogo/Palmeiras x Red Bull Bragantino/time/Red Bull Bragantino", {
+            method: 'GET',
+            headers: {
+              "Content-Type": "application/json",
+              "Authorization": `Bearer ${token}`,
+            },
+            redirect: 'follow',
+          });
+          if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            setDesfechos(data);
+          } else {
+            setError(`Erro ao buscar desfechos: ${response.statusText}`);
+          }
+        } catch (error) {
+          setError(`Erro na requisição: ${error.message}`);
+        } finally {
+          setIsLoading(false);
+        }
+      };
+
+      fetchDesfechos();
+      fetchDestaques();
+      fetchLances();
+    }, []);
 
   return (
     <>
       <Header />
       <div className="container">
-        {/* Container principal para a página de Partidas */}
         <div className="visao-geral">
           <div className="campo-futebol">
             <div className="goal top-goal"></div>
@@ -57,17 +106,22 @@ export default function Partidas() {
             <div className="corners"></div>
             <div className="lines"></div>
           </div>
-          <div className="destaques">Destaques</div>
-          <div className="envolvidos">Jogadores Envolvidos</div>
-          <div className="desfechos">Desfechos</div>
-        </div>
 
+          <div className="destaques">  
+            Destaques
+            data : {JSON.stringify(destaques)}
+          </div>
+
+          <div className="envolvidos">Jogadores Envolvidos</div>
+
+          <div class="chart-container">
+          </div>
+        </div>
         <div className="lista-lances">
-          Lista de Lances
-          data: {JSON.stringify(lances)}
+          Lista de Rupturas
+          data: {JSON.stringify(rupturas)}
         </div>
         <div className="video-container">
-          {/* Placeholder para o vídeo - substitua 'path_to_your_video.mp4' pelo caminho correto do seu vídeo */}
           <video controls>
             <source src="path_to_your_video.mp4" type="video/mp4" />
             Seu navegador não suporta vídeos.
