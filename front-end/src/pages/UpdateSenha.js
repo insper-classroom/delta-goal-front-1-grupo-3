a62242894f1c2d4bcd47ba7347236b3bbdcb546a
jonasbp <jonasbonfapelegrina@gmail.com>
2023-11-30 21:16:56 -0300

    Arruma requisicao de muda senha

--- front-end/src/pages/UpdateSenha.js
+++ front-end/src/pages/UpdateSenha.js
@@ -22,7 +22,7 @@ export default function UpdateSenha() {
       }
       const params = new URLSearchParams(window.location.search);
       const token = params.get('token');
-      const response = await fetch('https://sprint-deltago-5179309dcfcb.herokuapp.com/update-senha', {
+      const response = await fetch('https://sprint-deltago-5179309dcfcb.herokuapp.com//update-password', {
         method: 'PUT',
         headers: {
           'Content-Type': 'application/json',
