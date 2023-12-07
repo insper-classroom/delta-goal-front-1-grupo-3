a0ba46c1b64a37ca8011ab7be5e573022e34274e
leosfreitas <leonardosf@al.insper.edu.br>
2023-12-07 15:10:14 -0300

    estilizacao

--- front-end/src/pages/Partidas.js
+++ front-end/src/pages/Partidas.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from './Header'; 
+import './style/Partidas.css';
+
 const Partidas = () => {
   return (
-    <div>
-      <Header />
-        <h1>Partidas</h1>
-            <p><Link to="/rupturas">Rupturas</Link></p>
-            <p><Link to="/cruzamentos">Cruzamentos</Link></p>
+  <>
+    <Header />
+      <div className='partidas-wrapper'>
+        <div className='partidas-container'>
+        <img src="static\media\DeltaGoalPrincipal.9381cf8841f3dc93dc2f.png" />
+          <div className='partidas-content'>
+              <h2>Palmeiras X Red Bull Bragantino:</h2>
+                <p className='rupturas'><Link to="/rupturas">Rupturas</Link></p>
+                <p className='cruzamentos'><Link to="/cruzamentos">Cruzamentos</Link></p>
+          </div>
+        </div>
     </div>
+  </>
   );
 }
 
